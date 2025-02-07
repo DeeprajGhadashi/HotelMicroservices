d9b4f451134a17e5ac7838cfd4b996667bc4cedd
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2025-02-07 22:57:06 +0530

    Microservices Communication | User Service Communicate to Rating Service using REST TEMPLATE

--- UserService/src/main/java/com/hotel/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/hotel/user/service/UserServiceApplication.java
@@ -3,6 +3,9 @@ package com.hotel.user.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -10,5 +13,10 @@ public class UserServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
+	
+	 /* @Bean
+	    public RestTemplate restTemplate() {
+	        return new RestTemplate();
+	    }
+     */
 }
--- UserService/src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package com.hotel.user.service.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.hotel.user.service.entities.Rating;
 import com.hotel.user.service.entities.User;
 import com.hotel.user.service.exceptions.ResourceNotFoundException;
 import com.hotel.user.service.repositories.UserRepository;
@@ -17,6 +22,11 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private Logger logger= LoggerFactory.getLogger(UserServiceImpl.class);
+	
 	@Override
 	public User saveUser(User user) {
 		//generate unique userId
@@ -27,12 +37,36 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public List<User> getAllUser() {
-		return userRepository.findAll();
+	    List<User> users = userRepository.findAll();
+
+	    for (User user : users) {
+	        try {
+	            String url = "http://localhost:8083/ratings/users/" + user.getUserId();
+	            Rating[] ratingsArray = restTemplate.getForObject(url, Rating[].class);
+	            List<Rating> ratingsList = ratingsArray != null ? List.of(ratingsArray) : List.of();
+	            user.setRatings(ratingsList);
+	        } catch (Exception e) {
+	            logger.error("Error fetching ratings for user {}: {}", user.getUserId(), e.getMessage());
+	            user.setRatings(List.of()); // Set empty list if API call fails
+	        }
+	    }
+
+	    return users;
 	}
+
 
 	@Override
 	public User getUser(String userId) {
-		return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server !! :" + userId));
+		//get user from database with the help of user repository
+		User user=userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server !! :" + userId));
+	    // fetch rating of the above user from RATING SERVICE
+		//http://localhost:8083/ratings/users/b21bbc74-7dc0-40ad-8be3-653b79239f94 | calling with the http client 
+		//used REST TEMPLATE
+		ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId() , ArrayList.class);
+		logger.info("Rating for user {}",ratingsOfUser);
+		user.setRatings(ratingsOfUser);
+		
+		return user;
 	}
 
 	
