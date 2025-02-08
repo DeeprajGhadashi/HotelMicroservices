0097927bdd22b450c5d06ac6066fc7f8eabe2666
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2025-02-08 20:49:36 +0530

    Calling Two Microservices Together | User Service Communicate to Rating Service and Hotel Service using REST TEMPLATE

--- UserService/src/main/java/com/hotel/user/service/entities/Hotel.java
+++ UserService/src/main/java/com/hotel/user/service/entities/Hotel.java
@@ -1,6 +1,5 @@
 package com.hotel.user.service.entities;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,14 +9,10 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Rating {
+public class Hotel {
 
-	 private String ratingId;
-	 private String userId;
-	 private String hotelId;
-	 private int rating;
-	 private String feedback;
-	 
-	 private Hotel hotel;
-	 
+	private String id;
+	private String name;
+	private String location;
+	private String about;
 }
--- UserService/src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/hotel/user/service/services/impl/UserServiceImpl.java
@@ -1,15 +1,19 @@
 package com.hotel.user.service.services.impl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.hotel.user.service.entities.Hotel;
 import com.hotel.user.service.entities.Rating;
 import com.hotel.user.service.entities.User;
 import com.hotel.user.service.exceptions.ResourceNotFoundException;
@@ -37,36 +41,43 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public List<User> getAllUser() {
-	    List<User> users = userRepository.findAll();
-
-	    for (User user : users) {
-	        try {
-	            String url = "http://localhost:8083/ratings/users/" + user.getUserId();
-	            Rating[] ratingsArray = restTemplate.getForObject(url, Rating[].class);
-	            List<Rating> ratingsList = ratingsArray != null ? List.of(ratingsArray) : List.of();
-	            user.setRatings(ratingsList);
-	        } catch (Exception e) {
-	            logger.error("Error fetching ratings for user {}: {}", user.getUserId(), e.getMessage());
-	            user.setRatings(List.of()); // Set empty list if API call fails
-	        }
-	    }
-
-	    return users;
+		//implement RATING SERVICE CALL: USING REST TEMPLATE
+		return userRepository.findAll();
 	}
-
 
 	@Override
 	public User getUser(String userId) {
-		//get user from database with the help of user repository
-		User user=userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server !! :" + userId));
-	    // fetch rating of the above user from RATING SERVICE
-		//http://localhost:8083/ratings/users/b21bbc74-7dc0-40ad-8be3-653b79239f94 | calling with the http client 
-		//used REST TEMPLATE
-		ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId() , ArrayList.class);
-		logger.info("Rating for user {}",ratingsOfUser);
-		user.setRatings(ratingsOfUser);
-		
-		return user;
+	    // Get user from database with the help of user repository
+	    User user = userRepository.findById(userId)
+	            .orElseThrow(() -> new ResourceNotFoundException("User with given id is not found on server !! :" + userId));
+
+	    // Fetch ratings of the user from RATING SERVICE
+	    Rating[] ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), Rating[].class);
+	    
+	    logger.info("Rating for user {}", (Object) ratingsOfUser);  // Explicit cast to avoid array printing issue
+
+	    // Convert array to list safely
+	    List<Rating> ratings = (ratingsOfUser != null) ? Arrays.asList(ratingsOfUser) : new ArrayList<>();
+
+	    List<Rating> ratingList = ratings.stream().map(rating -> {
+	        // API call to hotel service to get the hotel
+	        ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://localhost:8082/hotels/" + rating.getHotelId(), Hotel.class);
+	        Hotel hotel = forEntity.getBody();
+	        
+	        logger.info("Response status code: {}", forEntity.getStatusCode());
+
+	        // Set the hotel to rating if not null
+	        if (hotel != null) {
+	            rating.setHotel(hotel);
+	        }
+
+	        return rating;
+	    }).collect(Collectors.toList());
+
+	    // Set ratings to user
+	    user.setRatings(ratingList);
+
+	    return user;
 	}
 
 	
